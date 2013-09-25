d42dfe4bc0d929c244e7a1d5ca3746815939c124
えちょ <dot.station@gmail.com>
2013-09-25 17:45:11 +0900

    パッケージ内容の検討

---
+++ src2/Test/UuiDB/CreateTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Util;
+
+namespace Test.UuiDB
+{
+
+    [TestClass]
+    public class CreateTest1
+    {
+        private static string TestDir { get; set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var guid = Guid.NewGuid();
+            TestDir = Path.GetFullPath(guid.ToBase64UrlString());
+            Directory.CreateDirectory(TestDir);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Directory.Delete(TestDir, true);
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
