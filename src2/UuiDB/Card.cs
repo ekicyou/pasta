6c9df64cd00a7622740147d7cba0c99d91af5695
えちょ <dot.station@gmail.com>
2013-09-26 18:01:28 +0900

    データベースの基本構造の実装中

---
+++ src2/UuiDB/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace UuiDB
+{
+    /// <summary>
+    /// データベースに保存される情報。
+    /// </summary>
+    public sealed class Card
+    {
+        /// <summary>一意なID(GUID)</summary>
+        public Guid ID { get; set; }
+
+        /// <summary>ヘッダ情報</summary>
+        public JObject Header { get; set; }
+
+        /// <summary>本体テキスト</summary>
+        public String Body { get; set; }
+
+        /// <summary>Blob情報</summary>
+        public byte[] Blob { get; set; }
+
+
+
+    }
+}
---
+++ src2/UuiDB/Stores/SystemConstStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UuiDB.Stores
+{
+    /// <summary>
+    /// Systemが規定値として持つ、固定値情報のストア。
+    /// 内容は変更できません。
+    /// </summary>
+    class SystemConstStore
+    {
+
+    }
+}
