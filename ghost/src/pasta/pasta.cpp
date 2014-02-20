35c25eee6858db39d1d45ee11920195acd474c97
えちょ <dot.station@gmail.com>
2014-02-20 18:33:15 +0900

    調整

--- ghost/src/pasta/pasta.cpp
+++ ghost/src/pasta/pasta.cpp
@@ -4,9 +4,14 @@
 #include "stdafx.h"
 #include "pasta.h"
 
-namespace pasta{
+pasta::App::App(const HINSTANCE hinst, const std::string& loaddir){
+	this->hinst = hinst;
 
+}
+pasta::App::~App(void){
 
+}
+bool pasta::App::request(const std::string& request, std::string& response){
 
-
+	return false;
 }
--- ghost/src/pasta/pasta.h
+++ ghost/src/pasta/pasta.h
@@ -2,31 +2,28 @@
 
 #include <windows.h>
 #include <string>
+#include "util.h"
 
-namespace pasta { 
+namespace pasta{
+
+	enum CharMode {
+		MODE_ANSI,
+		MODE_UTF_8,
+	};
 
 	class App
 	{
 	public:
-		enum CharMode {
-			MODE_ANSI,
-			MODE_UTF_8,
-		};
 
 	private:
 		HINSTANCE hinst;
 		std::wstring loaddir;
 		CharMode charMode;
 
-		void Init(void);
-
-
 	public:
 		App(const HINSTANCE hinst, const std::string& loaddir);
 		~App(void);
 		bool request(const std::string& request, std::string& response);
 	};
-
-
 
 }
