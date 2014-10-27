821bc908e43339ba660381927de8cf334a8b5184
������ <dot.station@gmail.com>
2014-10-27 16:53:11 +0900

    不要になったのでetcプロジェクトを除外

--- src/etc/copyItem.bat
+++
@@ -1,7 +0,0 @@
-setlocal
-pushd %~dp0
-
-  robocopy /MIR duktape\ %1\duktape\
-  robocopy /MIR lib\     %1\lib\
-  robocopy /MIR dic\     %1\dic\
-  popd
--- src/etc/lib/shiori.js
+++
@@ -1,8 +0,0 @@
-// ルートプログラム
-// 必要なスクリプトを読み込み、
-//  [Shiori.load(dir)]
-//  [Shiori.unload()]
-//  [Shiori.get(req)]
-//  [Shiori.notify(req)]
-// の各関数をフックする。
-
