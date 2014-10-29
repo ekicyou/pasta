d39d577394d408412de268737a2791d5c58e5e68
������ <dot.station@gmail.com>
2014-10-29 21:25:11 +0900

    現状でコミット

--- src/domain/ts/shiori_api/request.ts
+++ src/domain/ts/shiori_api/request.ts
@@ -5,6 +5,26 @@ import IF = require('../interfaces');
 
 var logger = new Duktape.Logger();
 
+
+/// リクエスト分解
+
+/*
+#define IDENTIFIER      T("([$a-zA-Z_][$0-9a-zA-Z_-]*)")
+#define CRLF            T("\\r\\n")
+
+#define SHIORI_VER      T("SHIORI/3.0")
+#define SHIORI_HEADER   IDENTIFIER T(" ") SHIORI_VER CRLF
+#define SHIORI_VALUE    IDENTIFIER T(": (.*?)") CRLF
+
+#define SHIORI_REQUEST  T("^") SHIORI_HEADER T("(") SHIORI_VALUE T(")*") CRLF T("$")
+
+
+*/
+
+
+
+
+/// リクエスト管理
 export class request implements IF.shiori_request{
     public constructor(text: string, res_func: (res: string) => void) {
         this.raw = text;
