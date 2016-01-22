615b6c3513f68533b1be95c14934b6f263ee9c34
えちょ <dot.station@gmail.com>
2016-01-22 10:07:04 +0900

    node系で実装すべき

--- check.bat
+++
@@ -1,3 +0,0 @@
-setlocal
-pushd %~dp0
-  bin\pretzel taste --source=site --destination=..\bake --port 5000
--- init.bat
+++
@@ -1,7 +0,0 @@
-setlocal
-pushd %~dp0
-  rmdir /Q /S site
-  rmdir /Q /S site
-  rmdir /Q /S site
-      bin\pretzel create --source=site --withproject --template=razor  --wiki
-  rem bin\pretzel create --source=site --withproject --template=liquid
