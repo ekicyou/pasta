9f8f3aad66569d9e1514df5e80de0d7cbdf52632
ekicyou <dot.station@gmail.com>
2026-01-06 17:57:24 +0900

    feat(ctx): 環境オブジェクトの新規作成機能を追加 refactor(actor): アクター作成APIを整理し、モジュール化

--- crates/pasta_lua/scripts/pasta/shiori/actor.lua
+++ crates/pasta_lua/scripts/pasta/shiori/actor.lua
@@ -6,8 +6,10 @@
 --- @field spot integer 立ち位置（０以上）
 --- @field action Action アクションオブジェクト
 --- @field has_init_script boolean 初期化スクリプトが未実行ならtrue
+--- @field [string] string 動的定義
 local IMPL = {}
 
+local MOD = {}
 
 --- 立ち位置を設定する。
 --- @param spot integer 立ち位置（０以上）
@@ -23,7 +25,6 @@ function IMPL.start_action(self, action)
     action.now_actor = nil
     self.has_init_script = true
 end
-
 
 function IMPL.change_actor(self)
     if self.now_actor == self then
@@ -44,7 +45,7 @@ local spot_counter = 0
 
 -- アクターオブジェクトの新規作成
 -- @return Actor アクターオブジェクト
-local function new(name)
+function MOD.new_actor(name)
     local actor = {}
     actor.name = name
     actor.spot = spot_counter
@@ -53,4 +54,18 @@ local function new(name)
     return actor
 end
 
-return new
+local actors = {}
+
+--- アクターオブジェクトを取得または新規作成する。
+--- @param name string アクター名
+--- @return Actor
+function MOD.create_actor(name)
+    if actors[name] then
+        return actors[name]
+    end
+    local actor = MOD.new_actor(name)
+    actors[name] = actor
+    return actor
+end
+
+return MOD
--- crates/pasta_lua/scripts/pasta/shiori/init.lua
+++ crates/pasta_lua/scripts/pasta/shiori/init.lua
@@ -1,16 +1,9 @@
 --- SHIORI.DLL用 パスタモジュール
 local MOD = {}
-local new_actor = require("pasta.shiori.actor")
+local ACTOR = require("pasta.shiori.actor")
 
-local actors = {}
 
---- アクターオブジェクトを新規作成する。
---- @param name string アクター名
---- @return Actor アクターオブジェクト
-function MOD.create_actor(name)
-    local actor = new_actor(name)
-    actors[name] = actor
-    return actor
-end
+MOD.create_actor = ACTOR.create_actor
+
 
 return MOD
